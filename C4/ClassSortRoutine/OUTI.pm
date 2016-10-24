74145f4aab9cc699054d0ff93fd94c8bb1ce4cff
Pasi Korkalo <jp@pk.dy.fi>
2016-10-24 15:24:19 +0300

    KD-1459 - Kohasuomi class sort routines
    Two new classification sorting routines for Koha-Suomi libraries:
    
    OUTI  - Make cn_sort fields by dropping off the first part of signum
            like this: AIK 84.2 PÄÄ -> 84.2 PÄÄ. This will (probably)
            work for all ex-Pallas libraries.
    
    LUMME - Copy itemcallnumbers to cn_sort as they are, without changes.
    
    Two scripts are included (in misc/) for updating the existing item
    records in the database to follow the above two cn_sort schemes:
    OUTIcn_sortFix.pl and LUMMEcn_sortFix.pl.

---
+++ C4/ClassSortRoutine/OUTI.pm
@@ -0,0 +1,65 @@
+package C4::ClassSortRoutine::OUTI;
+
+# Copyright (C) 2007 LibLime
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+C4::ClassSortRoutine::OUTI - OUTI call number sorting key routine
+
+=head1 SYNOPSIS
+
+use C4::ClassSortRoutine qw( GetClassSortKey );
+
+my $cn_sort = GetClassSortKey('OUTI', $cn_class, $cn_item);
+
+=head1 FUNCTIONS
+
+=head2 get_class_sort_key
+
+  my $cn_sort = C4::ClassSortRoutine::OUTI::OUTI($cn_class, $cn_item);
+
+Generates sorting key using the following rules:
+
+* Concatenates class and item part skipping the location part (since doesn't make any sense in OUTI).
+* Removes leading and trailing whitespace.
+
+=cut
+
+sub get_class_sort_key {
+    my ($cn_class, $cn_item) = @_;
+
+    $cn_class = '' unless defined $cn_class;
+    $cn_item  = '' unless defined $cn_item;
+    my $key = uc "$cn_class $cn_item";
+    $key =~ s/^\s+//;
+    $key =~ s/\s+$//;
+    $key =~ s/^.*? //;
+    return $key;
+}
+
+1;
+
+=head1 AUTHOR
+
+Koha Development Team <http://koha-community.org/>
+Broken by Koha-Suomi Oy/OUTI-Libraries <http://koha-suomi.fi/>
+
+=cut
