ccb2e1368b0e661be712eaa067579b9d222bb5d8
Johanna Raisa <johanna.raisa@gmail.com>
2022-01-18 13:39:23 +0200

    KD-4792: (squashable) remove unused rows

--- Koha/Procurement/OrderProcessor.pm
+++ Koha/Procurement/OrderProcessor.pm
@@ -109,8 +109,6 @@ sub process{
                 $basketNumber = $basketHelper->getBasket($bookseller, $authoriser, $basketName );
                 $orderId = $orderCreator->createOrder($copyDetail, $item, $order, $biblio, $basketNumber);
                 for(my $i = 0; $copyQty > $i; $i++ ){
-                    $self->advanceBarcodeValue();
-                    $barCode = $self->getBarcodeValue();
                     $itemId = $self->createItem($copyDetail, $item, $order, $barCode, $biblio, $biblioitem);
                     $orderCreator->createOrderItem($itemId, $orderId);
                 }
