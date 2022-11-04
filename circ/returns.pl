20cb7ff9aab06c28bb02765c645d9cb18e11518d
lmstrand <lmstrand@gmail.com>
2022-11-04 10:20:28 +0200

    G238 Use itemtype authorised value descriptions

--- circ/returns.pl
+++ circ/returns.pl
@@ -598,6 +598,8 @@ foreach ( sort { $a <=> $b } keys %returneditems ) {
 
         my $biblio = $item->biblio;
         my $biblioitem = $item->biblioitem;
+        
+            my $authorised_value_mtype = Koha::AuthorisedValues->get_description_by_koha_field({ kohafield => 'biblioitems.itemtype', authorised_value => $biblioitem->itemtype });
 
         # FIXME pass $item to the template and we are done here...
         $ri{itembiblionumber}    = $biblio->biblionumber;
@@ -608,7 +610,7 @@ foreach ( sort { $a <=> $b } keys %returneditems ) {
         $ri{itemauthor}          = $biblio->author;
         $ri{itemcallnumber}      = $item->itemcallnumber;
         $ri{dateaccessioned}     = $item->dateaccessioned;
-        $ri{recordtype}          = $biblioitem->itemtype;
+        $ri{recordtype}          = $authorised_value_mtype->{lib};
         $ri{itemtype}            = $item->itype;
         $ri{itemnote}            = $item->itemnotes;
         $ri{itemnotes_nonpublic} = $item->itemnotes_nonpublic;
